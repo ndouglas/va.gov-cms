ba84113bd876c5552d68ca02e3d163b270b2cd92
Nathan Douglas <nathan.douglas@agile6.com>
2020-10-29 14:09:06 -0500

    Adds a Blackfire profile to the PerformanceLoginTest.

--- tests/phpunit/PerformanceLoginTest.php
+++ tests/phpunit/PerformanceLoginTest.php
@@ -3,11 +3,14 @@
 namespace tests\phpunit;
 
 use weitzman\DrupalTestTraits\ExistingSiteBase;
+use Traits\BlackfireTestTrait;
 
 /**
  * A test to confirm amount of nodes by type.
  */
 class LoginPerformance extends ExistingSiteBase {
+
+  use BlackfireTestTrait;
 
   /**
    * A test method to determine the amount of time to load the Login page.
@@ -49,6 +52,53 @@ class LoginPerformance extends ExistingSiteBase {
     $secs = number_format($microsecs, 3);
     $this->assertLessThan($benchmark, $secs, __METHOD__ . "\nOperation took " . $secs . " seconds which is longer than the benchmark of " . $benchmark . " seconds.\n");
 
+    $message = __METHOD__  . "\nOperation took " . $secs . " seconds compared to the benchmark of " . $benchmark . " seconds.\n";
+    fwrite(STDERR, print_r($message, TRUE));
+  }
+
+  /**
+   * A test method to determine the amount of time to load the Login page.
+   *
+   * @group performance
+   * @group all
+   * @group blackfire
+   *
+   * @requires extension blackfire
+   *
+   * @dataProvider benchmarkTime
+   */
+  public function testProfileLoginPerformance($benchmark) {
+
+    // Creates a user. Will be automatically cleaned up at the end of the test.
+    $author = $this->createUser();
+    $author->addRole('content_editor');
+    $author->save();
+
+    // Warm cache before testing so login test will be realistic.
+    $this->drupalLogin($author);
+    // Logout here because if we don't then drupalLogin() automatically calls
+    // drupalLogout() on the next drupalLogin() invocation, which is unrealistic.
+    $this->drupalLogout();
+
+    $this->prepareClient();
+
+    // Start timer.
+    $mtime = microtime();
+    $mtime = explode(" ", $mtime);
+    $mtime = $mtime[1] + $mtime[0];
+    $starttime = $mtime;
+
+    $this->drupalLogin($author);
+
+    // End timer.
+    $mtime = microtime();
+    $mtime = explode(" ", $mtime);
+    $mtime = $mtime[1] + $mtime[0];
+    $endtime = $mtime;
+    $microsecs = ($endtime - $starttime);
+
+    // Test assertion.
+    $secs = number_format($microsecs, 3);
     $message = __METHOD__  . "\nOperation took " . $secs . " seconds compared to the benchmark of " . $benchmark . " seconds.\n";
     fwrite(STDERR, print_r($message, TRUE));
   }
